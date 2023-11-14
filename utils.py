f7dc7be1ecd7c9baed11782ad13f51eaccea937b
Theeeccoo <thiagodecampos1@gmail.com>
2023-11-14 15:54:30 -0300

    ADDING: Cell_id information in tuple

--- utils.py
+++ utils.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
-#################################################
+#######################################################
 # Method to filter information from desired image
 # @param: fileName
-# returns: Tuple with nucleus_x and nucleus_y
-#################################################
+# returns: Tuple with nucleus_x, nucleus_y and cell_id
+#######################################################
 
-def getCoordinates(fileName):
+def getCellData(fileName):
     df = pd.read_csv("./data/classifications.csv")
     filtered = df[df['image_filename'] == fileName]
-    tuples = list(filtered[['nucleus_x', 'nucleus_y']].apply(tuple, axis=1))
+    tuples = list(filtered[['nucleus_x', 'nucleus_y', 'cell_id']].apply(tuple, axis=1))
     return tuples
